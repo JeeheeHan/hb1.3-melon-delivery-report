52a80171fa3033bbacfb1199450478c9d94dc95e
Jenny <han.jennyjh@outlook.com>
2021-02-25 05:59:57 +0000

    first version by making produce summary

--- produce_summary.py
+++ produce_summary.py
@@ -1,3 +1,30 @@
+file_one = open("um-deliveries-20140519.txt")
+file_two = open("um-deliveries-20140520.txt")
+file_three = open("um-deliveries-20140521.txt")
+
+def produce_summary(file_number):
+    """Return log of delivery information"""
+    for line in file_number:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    file_number.close()
+
+print("Day 1")
+produce_summary(file_one)
+print("Day 2")
+produce_summary(file_two)
+print("Day 3")
+produce_summary(file_three)
+
+
+"""
 print("Day 1")
 the_file = open("um-deliveries-20140519.txt")
 for line in the_file:
@@ -42,3 +69,4 @@ for line in the_file:
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
 the_file.close()
+"""
